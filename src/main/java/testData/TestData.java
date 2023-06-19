b1795cc817ad4fb3e4b0f80205d1416e36eedb10
Aleksandar Gjorgijovski <aleksandar.gjorgijovski@iwconnect.com>
2023-06-19 16:14:32 +0200

    first

---
+++ src/main/java/testData/TestData.java
@@ -0,0 +1,114 @@
+package testData;
+
+public class TestData {
+	//Users
+	public String validEmail ="aleksandar@hotmail.com";
+	public String validEmail1 ="1aleksandar@hotmail.com";
+	public String validEmail2 ="2aleksandar@hotmail.com";
+	public String validEmail3 ="3aleksandar@hotmail.com";
+	public String validEmail4 ="4aleksandar@hotmail.com";
+	public String validEmail5 ="5aleksandar@hotmail.com";
+	public String validEmail6 ="6aleksandar@hotmail.com";
+	public String emptyEmail = "";
+	public String emptyPassword = "";
+	public String emptyConfirmPassword = "";
+
+	public String invalidEmail = "test@test.com";
+	public String incorrectEmail = "123456";
+	
+	public String validDay = "13";
+	public String validMonth = "8";
+	public String validYear = "1990";
+	
+	public String validPassword = "demo123";
+	public String validConfrimPassword = "demo123";
+	public String invalidPassword = "123456";
+	public String uncorrectPassword = "123";
+	
+	public String firstName = "Aleksandar";
+	public String lastName = "Gjorgijovski";
+	public String companyName = "IWConnect";
+	
+	public String validHatSizeMedium = "35";
+	
+	public String validQuantity = "6";
+	public String validQuantity5 ="5";
+	public String validSizeTxt = "9";
+	public String validSizeValue = "22";
+	public String validNikeTailwindSizeValue = "31";
+	public String validMacedoniaValue = "11";
+	public String validCityBitola = "Bitola";
+	public String validCityPrilep = "Prilep";
+	public String validAddress = "Novosadska 27";
+	public String validShipAddress = "Bitolska 11";
+	public String validZipCode = "7000";
+	public String validShipZipCode = "7500";
+	public String validPhoneNumber = "070233323";
+	public String validShipPhoneNumber = "075330250";
+	
+	public String validCardTypeVisa = "visa";
+	public String validCardNumber = "4444333322221111";
+	public String validCardCode = "123";
+	public String validExpireMonth = "5";
+	public String validExpireYear = "2023";
+	
+	
+		
+	//Titles
+	public String homePageTitle = "nopCommerce demo store";
+	public String loginPageTitle = "nopCommerce demo store. Login";
+	public String passwordRecoveryTitle = "nopCommerce demo store. Password Recovery";
+	public String registerPageTitle = "nopCommerce demo store. Register";
+	
+	public String computersPageTitle = "nopCommerce demo store. Computers";
+	public String softwarePageTitle= "nopCommerce demo store. Software";
+	public String windows8PageTitle= "nopCommerce demo store. Windows 8 Pro";
+	
+	//Filters
+	public String filterItemByCpu5 = "i5";
+	public String filterByMemory8 = "8gb";
+	
+	public String downloadedFile = "Night_Vision_1.txt";
+	
+	public String reviewTitle = "Lieca Rules";
+	public String reviewTxt = "Excelent Camera";
+	
+	
+	//search Data
+	public String searchItemNike = "nIkE";
+	public String searchItemApple = "apple";
+	public String invalidSearchData = "Le";
+	public String valueComNote = "3";
+	public String valueSearchComputersNotebooks = "3";
+	
+	public String invaildItemSourcePage = "Apple iCam";
+	public String validItemSourcePage = "Processor: 2.2 GHz Intel Pentium Dual-Core E2200" + " " + "RAM: 8GB" + " " + "HDD: 400 GB"+ " " + "OS: Vista Home";
+	public String validItemSourcePage1 = "RAM: 8GB";
+	public String validItemSourcePage2 = "Processor: 2.2 GHz Intel Pentium Dual-Core E2200";
+	public String SKUElegant = "EG_GEM_NL";
+	public String SKUFahrenheit = "FR_451_RB";
+	public String SKUFlowerGirlBracelet = "FL_GIRL_B";
+	public String SKUObeyPropagandaHat = "OB_HAT_PR";
+	
+	
+	//screenshot
+	public String screenshotName = "nopcommerce";
+	
+	//Rental dates
+	public String startMonth = "AUGUST";
+	public String endMonth = "SEPTEMBER";
+	public String startYear = "2023";
+	public String endYear = "2023";
+	public String startDay = "20";
+	public String endDay = "30";
+	
+	
+	public String xssAttack = "<script></script>";
+	public String blockPageTitle ="Attention Required! | Cloudflare";
+	public String sqlInjection = "\"Select * from product\"";
+	
+	public String processor2_2GHZ = "1";
+	public String processor2_5GHz = "2";
+	public String ram8GB = "5";
+	
+}
